f87cf4b02fa8bfcd1b84106641a1fb2d9bcb3e8e
Tom Arild Jakobsen <tajakobsen@gmail.com>
2025-02-06 12:42:30 +0100

    Add the Guillotine interface

--- README.md
+++ README.md
@@ -1,9 +1,73 @@
 # Feature Toggles
 
+This application provides an admin tool to interact with _feature toggles_ created by the [Feature Toggles Lib](https://github.com/ItemConsulting/lib-xp-feature-toggles).
+A feature toggle is a boolean flag that can be used in the code to enable or disable features in your XP-application.
+
+Common usages of feature toggles are:
+ - Merging partially implemented features into your codebase, but not exposing them to the end users
+ - A feature switch (kill switch?) that can be used by the admins/editors
+ - Enable a feature on the `"draft"` branch, but not on `"master"`.
 
 [![](https://repo.itemtest.no/api/badge/latest/releases/no/item/xp-feature-toggle)](https://repo.itemtest.no/#/releases/no/item/xp-feature-toggle)
 
 ![Feature toggle logo](src/main/resources/application.svg)
+
+## Usage
+
+### Application code
+
+Install [Feature Toggles Lib](https://github.com/ItemConsulting/lib-xp-feature-toggles) in your application to create
+new _feature toggles_ and check if the feature is enabled.
+
+_Example:_
+
+```typescript
+import { create, isEnabled } from "/lib/feature-toggles";
+
+// We recommend doing this in the main.{js,ts} file.
+create({
+  name: "my-feature",
+  enabled: false,
+  description: "Use new and experimental code for checkout form",
+});
+
+// ...later in the controller code
+
+if (isEnabled("my-feature")) {
+  // facilitate rendering of "my feature"
+}
+```
+
+> [!TIP]
+> See the [documentation](https://github.com/ItemConsulting/lib-xp-feature-toggles/blob/main/README.md) of the complete API-surface.
+
+### Headless
+
+This app also exposes a GraphQL-interface through the [Guillotine app](https://market.enonic.com/vendors/enonic/guillotine).
+
+The following fields are exposed:
+ - `isEnabled(spaceKey: string, featureKey: string, branch?: string, defaultValue?: string): boolean`
+ - `getFeature(spaceKey: string, featureKey: string, branch?: string): Feature`
+ - `getFeatures(spaceKey?: string, branch?: string): Feature[]`
+
+_Example:_
+
+```graphql
+query {
+  guillotine {
+    # Check if a feature is enabled (returns Boolean)
+    enableMyFeature: isEnabled(spaceKey: "com.example.app", featureKey: "my-feature")
+    
+    # List all features
+    getFeatures {
+      enabled
+      name
+      description
+      spaceKey
+    }
+  }
+}
+```
 
 ## Deploying
 
@@ -20,10 +84,10 @@ enonic project build
 Deploy locally for testing purposes:
 
 ```bash
-./gradlew publishToMavenLocal
+enonic project deploy
 ```
 
-## Deploy to Maven
+### Deploy to Maven
 
 ```bash
 ./gradlew publish -P com.enonic.xp.app.production=true
---
+++ src/main/resources/guillotine/guillotine.ts
@@ -0,0 +1,101 @@
+import { getFeature, getFeatures, isEnabled } from "/lib/feature-toggles";
+import type { GraphQL, Extensions } from "@enonic-types/guillotine";
+
+const OBJECT_TYPE_FEATURE = "Feature";
+const FIELD_IS_ENABLED = "isEnabled";
+const FIELD_GET_FEATURE = "getFeature";
+const FIELD_GET_FEATURES = "getFeatures";
+
+/**
+ * `GraphQLFeature` reimplements the `Feature` type to lock down the output shape.
+ * We want the build to fail if GraphQLFeature != Feature, so that it doesn't fail in the GraphQL-client.
+ */
+type GraphQLFeature = {
+  id: string;
+  name: string;
+  enabled: boolean;
+  description?: string;
+  createdDate: string;
+};
+
+export function extensions(graphQL: GraphQL): Extensions {
+  return {
+    types: {
+      [OBJECT_TYPE_FEATURE]: {
+        description: "A feature toggle that can be enabled or disabled.",
+        fields: {
+          id: {
+            type: graphQL.nonNull(graphQL.GraphQLID),
+          },
+          name: {
+            type: graphQL.nonNull(graphQL.GraphQLString),
+          },
+          enabled: {
+            type: graphQL.nonNull(graphQL.GraphQLBoolean),
+          },
+          spaceKey: {
+            type: graphQL.nonNull(graphQL.GraphQLString),
+          },
+          description: {
+            type: graphQL.GraphQLString,
+          },
+          createdDate: {
+            type: graphQL.GraphQLString,
+          },
+        },
+      },
+    },
+    creationCallbacks: {
+      HeadlessCms: (params): void => {
+        params.addFields({
+          [FIELD_IS_ENABLED]: {
+            type: graphQL.GraphQLBoolean,
+            args: {
+              featureKey: graphQL.nonNull(graphQL.GraphQLString),
+              spaceKey: graphQL.nonNull(graphQL.GraphQLString),
+              branch: graphQL.GraphQLString,
+              defaultValue: graphQL.GraphQLBoolean,
+            },
+          },
+
+          [FIELD_GET_FEATURE]: {
+            type: graphQL.reference(OBJECT_TYPE_FEATURE),
+            args: {
+              featureKey: graphQL.nonNull(graphQL.GraphQLString),
+              spaceKey: graphQL.nonNull(graphQL.GraphQLString),
+              branch: graphQL.GraphQLString,
+            },
+          },
+
+          [FIELD_GET_FEATURES]: {
+            type: graphQL.list(graphQL.reference(OBJECT_TYPE_FEATURE)),
+            args: {
+              spaceKey: graphQL.GraphQLString,
+              branch: graphQL.GraphQLString,
+            },
+          },
+        });
+      },
+    },
+    resolvers: {
+      HeadlessCms: {
+        [FIELD_IS_ENABLED]: (env): boolean => {
+          return isEnabled({
+            spaceKey: env.args.spaceKey,
+            featureKey: env.args.featureKey,
+            branch: env.args.branch,
+            defaultValue: env.args.defaultValue,
+          });
+        },
+
+        [FIELD_GET_FEATURE]: (env): GraphQLFeature | undefined => {
+          return getFeature(`/${env.args.spaceKey}/${env.args.featureKey}`, env.args.branch);
+        },
+
+        [FIELD_GET_FEATURES]: (env): GraphQLFeature[] => {
+          return getFeatures(env.args.spaceKey, env.args.branch);
+        },
+      },
+    },
+  };
+}
